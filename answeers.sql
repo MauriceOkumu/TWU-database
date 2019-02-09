8bf133ee5a900c7cc0bccfd03a3dcabd83997f70
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-09 06:31:51 -0600

    Querying database for members and returning them as count

--- answeers.sql
+++ answeers.sql
@@ -1,3 +1,10 @@
 --select * from member;
 --select name from member where member.id=(select book.id from book where title="The Hobbit");
 select name from member where id =(select member_id from checkout_item where book_id=(select book.id from book where title="The Hobbit"));
+
+--select all the names
+select * from member where not exists(
+select 1 from checkout_item where checkout_item.member_id=member.id);
+
+--not using exists keyword, select the count
+select count(*) from member where(select count(*) from checkout_item where checkout_item.member_id=member.id) < 1;

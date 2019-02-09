6645bd64bdc65bc12a5229d508732867e03952ae
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-09 07:55:39 -0600

    Query for updating database

--- answeers.sql
+++ answeers.sql
@@ -1,26 +1,42 @@
 --select * from member;
 --select name from member where member.id=(select book.id from book where title="The Hobbit");
-select name from member where id =(select member_id from checkout_item where book_id=(select book.id from book where title="The Hobbit"));
+select name from member where id =(
+select member_id from checkout_item where book_id = (
+select book.id from book where title = "The Hobbit"));
 
 --select all the names
 --select * from member where not exists(
 --select 1 from checkout_item where checkout_item.member_id=member.id);
 
 --Add me as member
-insert into member(id, name) values(85, "Maurice Okumu");
+--insert into member(id, name) values(85, "Maurice Okumu");
 
 --Add a book
-insert into book(id, title) values(56, "The Pragmatic Programmer");
+--insert into book(id, title) values(56, "The Pragmatic Programmer");
 
 --not using exists keyword, select the count
 select count(*) from member where(
-select count(*) from checkout_item where checkout_item.member_id=member.id) < 1;
+select count(*) from checkout_item where checkout_item.member_id = member.id) < 1;
 
 --movies not checked out
 select * from movie where(
-select count(*) from checkout_item where checkout_item.movie_id=movie.id) < 1;
+select count(*) from checkout_item where checkout_item.movie_id = movie.id) < 1;
 
 --books not checked out
 select * from book where(
-select count(*) from checkout_item where checkout_item.book_id=book.id) < 1;
+select count(*) from checkout_item where checkout_item.book_id = book.id) < 1;
 
+update member set id = 77 where name = "Maurice Okumu";
+update book set id = 57 where title = "The Pragmatic Programmer";
+
+--create a join in the checkout table
+--insert into checkout_item(member_id, book_id, movie_id) values(
+--(select id from member where name = "Maurice Okumu"),
+--(select id from book where title = "The Pragmatic Programmer", null)
+
+--);
+
+--checkout the book I added and verify
+select name from member where id = (
+select member_id from checkout_item where book_id = (
+select book.id from book where title = "The Pragmatic Programmer"));
